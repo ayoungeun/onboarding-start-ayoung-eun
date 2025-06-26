453fe207d72975f55c6056bcfa0f94a5e0d7c3cb
unknown <aeun@uwaterloo.ca>
2025-06-26 18:35:26 -0400

    project.v file was messed up

--- src/project.v
+++ src/project.v
@@ -6,7 +6,20 @@
 `default_nettype none
 
 module tt_um_uwasic_onboarding_ayoung_eun (
- // Add this inside the module block
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  // List all unused inputs to prevent warnings
   assign uio_oe = 8'hFF; // Set all IOs to output
   
   // Create wires to refer to the values of the registers
@@ -29,6 +42,7 @@ module tt_um_uwasic_onboarding_ayoung_eun (
   );
   // Add uio_in and ui_in[7:3] to the list of unused signals:
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+
       // Registers or data interface
 
     // Instantiate the SPI peripheral module
@@ -42,6 +56,5 @@ module tt_um_uwasic_onboarding_ayoung_eun (
         .outtopwm2(en_reg_out_15_8)
     );
 
-)
-
 endmodule
+
