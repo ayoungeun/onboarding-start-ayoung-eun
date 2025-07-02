c47e712330bd2d7c6889d55642b9127e7372a1bf
unknown <aeun@uwaterloo.ca>
2025-07-02 17:24:12 -0400

    fixing wrong timing

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -40,19 +40,21 @@ always @(posedge clk or negedge rst_n) begin
         falling_counter <= 0;
         transaction_ready <= 1'b0;  
         // omitted code
-    end else if (nCS_sync[1] == 1'b0 && nCS_sync[0] == 1'b0) begin
+    end else if (nCS_sync[1] == 1'b0 && rising_counter < 16 && falling_counter < 16) begin
         isthisin <= 1'b1;
         //(2nd oldest = HIGH) AND (1st oldest = LOW)
         //mode 0: reads COPI data on rising edge of SCLK
-        if (rising_counter < 16 && sclk_sync[1] == 1'b1 && sclk_sync[0] == 1'b0) begin
+        if (sclk_sync[1] == 1'b1 && sclk_sync[0] == 1'b0) begin
             spi_buf <= {spi_buf[14:0], COPI_sync[1]}; // Shift in the COPI data bit
          $display("Shifted in: spi_buf=%h, rising_counter=%d, COPI=%b", spi_buf, rising_counter, COPI_sync[1]);
             rising_counter <= rising_counter + 1;
         //(2nd oldest = LOW) AND (1st oldest = HIGH)
         //mode 0: shift out COPI data on falling edge of SCLK
-        end else if (falling_counter < 16 && sclk_sync[1] == 1'b0 && sclk_sync[0] == 1'b1) begin
+        end else if (sclk_sync[1] == 1'b0 && sclk_sync[0] == 1'b1) begin
             falling_counter <= falling_counter + 1;
-        end else if (rising_counter == 16 && falling_counter == 16) begin
+        end 
+        
+        else if (rising_counter == 16 && falling_counter == 16) begin
             // Transaction is complete after 16 bits
         end
 
