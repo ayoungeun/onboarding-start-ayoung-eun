1df3bb239d61678da7e0ec39bc0711dd10325975
unknown <aeun@uwaterloo.ca>
2025-07-09 11:28:58 -0400

    for all

--- test/test.py
+++ test/test.py
@@ -213,10 +213,11 @@ async def test_pwm_freq(dut):
 
     dut._log.info("Let frequency test begin")
 
+    for bit in range(8):
     try:
-        t1 = await detect_rising_edge(dut, dut.uo_out[0], timeout=1000, bit=0)
-        t2 = await detect_falling_edge(dut, dut.uo_out[0], timeout=1000, bit=0)
-        t3 = await detect_rising_edge(dut, dut.uo_out[0], timeout=1000, bit=0)
+        t1 = await detect_rising_edge(dut, dut.uo_out[0], timeout=10000, bit=bit)
+        t2 = await detect_falling_edge(dut, dut.uo_out[0], timeout=10000, bit=bit)
+        t3 = await detect_rising_edge(dut, dut.uo_out[0], timeout=10000, bit=bit)
         dut._log.info(f"Got {t1} ns and {t3} ns")
         period_ns = t3 - t1
         freq = 1e9 / period_ns
